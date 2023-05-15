c99e47d7a7bcdf6a6094ed7041184c4c64f0ca72
HuyPC03028 <huyvncst123@gmail.com>
2023-05-15 12:55:23 +0700

    up project
    up project

---
+++ src/main/java/com/poly/main/Java5HousewareApplication.java
@@ -0,0 +1,13 @@
+package com.poly.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5HousewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java5HousewareApplication.class, args);
+	}
+
+}
